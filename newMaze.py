939377a7edf60c7a17eeaeaced40800275986185
Mitchell Foley <mitch.foley@mail.com>
2015-03-31 14:01:04 -0400

    Gets stuck after first greedy walk

--- newMaze.py
+++ newMaze.py
@@ -17,13 +17,14 @@ class myStack():
     def isEmpty(self):
 
         if len(self.stack) == 0:
-            return true
+            return True
         else:
             return False
 
 class Builder():
 
     def __init__(self,m,n,start):
+        random.seed()
         self.maze = m
         self.position = start
         self.cell = maze[start[0]][start[1]]
@@ -38,17 +39,18 @@ class Builder():
         
         
 
-    def step(self):
-        for y in range(1,self.n+2):
-            for x in range(1,self.n+2):
-                self.cell = maze[y][x]
-                i = random.choice(tuple(self.cell.neighbours))
-                self.cell = maze[self.cell.neighbours[i][0]][self.cell.neighbours[i][1]]
-                self.path.push((y,x))
-                self.position=(y,x)
-                self.cell.visited = True
-                maze[self.position[0]][self.position[1]].visited = True
-            
+    def step(self,y,x):
+        self.cell = maze[y][x]
+        i = random.choice(tuple(self.cell.neighbours))
+        if maze[self.cell.neighbours[i][0]][self.cell.neighbours[i][1]].visited == False:
+            self.cell = maze[self.cell.neighbours[i][0]][self.cell.neighbours[i][1]]
+            self.path.push((self.cell.y,self.cell.x))
+            self.position=(self.cell.y,self.cell.x)
+            self.cell.visited = True
+            maze[self.position[0]][self.position[1]].visited = True
+            print (self.position)
+        return self.position
+        
                 
 
     def draw(self):
@@ -83,14 +85,32 @@ class cell():
     def refreshNeighbours(self):
         x = self.x
         y = self.y
-        if (x-1) < 1:
-            del(self.neighbours['W'])
-        if (x+1) > self.n:
-            del(self.neighbours['E'])
-        if (y-1) < 1:
-            del(self.neighbours['S'])
-        if (y+1) > self.n:
-            del(self.neighbours['N'])
+        numNeighbours = len(self.neighbours)
+        try:
+            if (x-1) < 1:
+                del(self.neighbours['W'])
+            if (x+1) > self.n:
+                del(self.neighbours['E'])
+            if (y-1) < 1:
+                del(self.neighbours['S'])
+            if (y+1) > self.n:
+                del(self.neighbours['N'])
+            temp = self,neighbours
+            for i in {'N','E','W','S'}:
+                if maze[self.neighbours[i][0]][self.neighbours[i][1]].visited == True or self.neighbours[i][0] > self.n or self.neighbours[i][0] < 0 or self.neighbours[i][1] > self.n or self.neighbours[i][1] < 0 :
+                    del(self.neighbours[i])
+            print(self.neighbours)
+            if self.neighbours == {}:
+                return True
+            else:
+                self.neighbours = temp
+                return False
+        except:
+            pass
+        if numNeighbours <= 0:
+            return True
+        else:
+            return False
     
 
 n = int(input("Enter maze size: "))
@@ -100,9 +120,20 @@ for y in range(n+2):
     for x in range(n+2):
         C.append(cell(y,x,n))
     maze.append(C)
-
+maze[1][1].visited = True
 Cam = Builder(maze,n,(1,1))
-Cam.step()
+working = True
+y=1
+x = 1
+isStuck = False
+while working:
+    isStuck = maze[y][x].refreshNeighbours()
+    p = Cam.step(y,x)
+    y = p[0]
+    x = p[1]
+    print(isStuck,maze[y][x].neighbours)
+    if isStuck:
+        working = False
 print (Cam.path.stack)
 
 
