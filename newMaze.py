ff3baf09559a390246882e786e8d0419ac094b7f
Mitchell Foley <mitch.foley@mail.com>
2015-03-31 12:06:13 -0400

    Builder Class

--- newMaze.py
+++ newMaze.py
@@ -1,3 +1,5 @@
+import random
+import turtle
 class myStack():
 
     def __init__(self,initial = []):
@@ -18,8 +20,49 @@ class myStack():
             return true
         else:
             return False
-    
 
+class Builder():
+
+    def __init__(self,m,n,start):
+        self.maze = m
+        self.position = start
+        self.cell = maze[start[0]][start[1]]
+        self.n = n
+        self.path = myStack([(1,1)])
+        self.turt = turtle.Turtle()
+        self.turt.ht()
+        self.turt.title = "Maze Builder Path"
+        self.turt.goto(1,1)
+        self.turt.home = (1,1)
+        
+        
+        
+
+    def step(self):
+        for y in range(1,self.n+2):
+            for x in range(1,self.n+2):
+                self.cell = maze[y][x]
+                i = random.choice(tuple(self.cell.neighbours))
+                self.cell = maze[self.cell.neighbours[i][0]][self.cell.neighbours[i][1]]
+                self.path.push((y,x))
+                self.position=(y,x)
+                self.cell.visited = True
+                maze[self.position[0]][self.position[1]].visited = True
+            
+                
+
+    def draw(self):
+        self.scr = turtle.Screen()
+        for p in self.path.stack:
+            if p[0] > 0 and p[0] <= self.n and p[1] > 0 and p[1] <= self.n:
+                self.turt.pendown()
+            else:
+                self.turt.penup()
+
+            self.turt.setposition(50*p[1],50*p[0])   
+            
+
+                
 class cell():
 
     def __init__(self,y,x,n):
@@ -58,13 +101,8 @@ for y in range(n+2):
         C.append(cell(y,x,n))
     maze.append(C)
 
-class Builder():
+Cam = Builder(maze,n,(1,1))
+Cam.step()
+print (Cam.path.stack)
 
-    def __init__(self,m,n):
-        self.maze = m
-        self.n = n
-        self.path = myStack((1,1))
 
-    def step(self):
-        for y in range(
-    
